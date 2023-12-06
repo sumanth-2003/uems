79dfb9dd3cf6496c88bafe2215e022ac1e834ba8
sumanth-2003 <nakirikantisumanth@gmail.com>
2023-12-06 11:41:33 +0530

    changed timetable code

--- src/components/Timetable.js
+++ src/components/Timetable.js
@@ -1,9 +1,39 @@
-import React from 'react'
 
 const Timetable = () => {
-  return (
-    <div>Timetable</div>
-  )
-}
+  // Replace this data with your actual schedule
+  const schedule = [
+    { time: '9:00 AM', monday: 'Math', tuesday: 'English', wednesday: 'Science' },
+    { time: '10:00 AM', monday: 'History', tuesday: 'Math', wednesday: 'Physical Education' },
+    // Add more entries as needed
+  ];
 
-export default Timetable
+  return (
+    <div>
+      <h1>Weekly Time Table</h1>
+      <table border="1">
+        <thead>
+          <tr>
+            <th>Time</th>
+            <th>Monday</th>
+            <th>Tuesday</th>
+            <th>Wednesday</th>
+            {/* Add more days as needed */}
+          </tr>
+        </thead>
+        <tbody>
+          {schedule.map((entry, index) => (
+            <tr key={index}>
+              <td>{entry.time}</td>
+              <td>{entry.monday}</td>
+              <td>{entry.tuesday}</td>
+              <td>{entry.wednesday}</td>
+              {/* Add more cells for additional days */}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Timetable;
